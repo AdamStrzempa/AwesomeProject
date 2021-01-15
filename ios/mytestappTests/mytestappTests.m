c486de5a18c2df4939471582164953b47f61358c
AdamStrzempa <adams@reactpoland.com>
2021-01-15 16:52:13 +0100

    after rename

--- ios/mytestappTests/mytestappTests.m
+++ ios/mytestappTests/mytestappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface mytestappTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation mytestappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
